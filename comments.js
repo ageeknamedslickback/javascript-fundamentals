4d447110ca9df930545d2d844f52af764863cda3
Dumbledore <mathenge@healthcloud.co.ke>
2021-04-11 18:54:01 +0300

    JS syntax bascics and types

--- comments.js
+++
@@ -1,9 +0,0 @@
-// This file is  for writing JS comments
-
-/*
-Funny but. We start from baby steps and build
-up from there on...
-
-Oh Yeah, also got this course from 
-https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/
-*/
